28f5f2d6ee72a4fe92d9676f63768a5c99a08a8d
Spikesnaveen <naveensrinivasan984189@gmail.com>
2023-11-05 00:51:47 +0530

    ramana solutions icon added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import rslogo from './rs2.png';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={rslogo} className="App-logo" alt="logo" />
+        <h3 className="App-founder">
+          Founders:
+        </h3>
+        <p className="App-naveen-name">
+          Rajapandian Chellimuthu 
+          <br/>
+          Manigandan Sivaramakrishnan
+          <br/>
+          Naveen Srinivasan
+          <br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
