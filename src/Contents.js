b2fb07f42de368764d4c8343e6262cbacb42506b
Spikesnaveen <naveensrinivasan984189@gmail.com>
2023-11-26 19:16:58 +0530

    abstract

--- src/Contents.js
+++ src/Contents.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { BsFillTrashFill } from "react-icons/bs";
+import { ItemsList } from "./ItemsList";
 
 const Contents = ({ items, handleChecked, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id} className="item">
-              <input
-                type="checkbox"
-                checked={item.checked}
-                onChange={() => handleChecked(item.id)}
-              />
-              <label>{item.item}</label>
-              <BsFillTrashFill
-                role="button"
-                onClick={() => handleDelete(item.id)}
-                tabIndex="0"
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemsList
+          items={items}
+          handleChecked={handleChecked}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p> Your List is empty</p>
       )}
---
+++ src/LineItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsFillTrashFill } from "react-icons/bs";
+
+const LineItem = ({item, handleChecked, handleDelete}) => {
+  return (
+    <li key={item.id} className="item">
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleChecked(item.id)}
+              />
+              <label>{item.item}</label>
+              <BsFillTrashFill
+                role="button"
+                onClick={() => handleDelete(item.id)}
+                tabIndex="0"
+              />
+            </li>
+  )
+}
+
+export default LineItem
